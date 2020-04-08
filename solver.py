ca370866fa801457d171ab4f812f2f4f7ceeb9a7
Gabriel Zanetti <gabrielztk@al.insper.edu.br>
2020-04-08 20:18:35 -0300

    FIX

--- solver.py
+++ solver.py
@@ -42,13 +42,13 @@ class Bar:
         return deformation, tension, internal_forces
     
 
-class Solver:
+class Structure:
 
     def __init__(self):
         self.solved = False
 
     def load(self, fille_name):
-        data = importa(fille_name + ".xlsx")
+        data = importa(fille_name)
         self.nodes_number = data[0]
         self.nodes_matrix = data[1]
         self.members_number = data[2]
@@ -60,17 +60,12 @@ class Solver:
         self. colapse_conditions = data[8]
         self.solved = False
 
-    def plot(self):
-        plota(self.nodes_matrix, self.incidence_matrix)
-
-    def solve(self):
-
         nodes = []
         for node in range(self.nodes_number):
             nodes.append(Node(node, self.nodes_matrix[0][node], self.nodes_matrix[1][node]))
         
-        bars = []
-        global_rigidity_matrix = np.zeros((self.nodes_number * 2, self.nodes_number * 2))
+        self.bars = []
+        
         
         self.bar_lenghts = np.zeros((self.members_number, 1))
         self.bar_weights = np.zeros((self.members_number, 1))
@@ -80,7 +75,15 @@ class Solver:
             self.bar_lenghts[member][0] = bar.lenght
             self.bar_weights[member][0] = bar.weight
             self. weight[0,0] += bar.weight
-            bars.append(bar)
+            self.bars.append(bar)
+
+    def plot(self):
+        plota(self.nodes_matrix, self.incidence_matrix)
+
+    def solve(self):
+
+        global_rigidity_matrix = np.zeros((self.nodes_number * 2, self.nodes_number * 2))
+        for bar in self.bars:
             for line in range(4):
                 for column in range(4):
                     global_rigidity_matrix[bar.liberty_degree[line]][bar.liberty_degree[column]] += bar.rigidity_matrix[line][column]
@@ -91,7 +94,7 @@ class Solver:
         contour_global_rigidity_matrix = np.delete(contour_global_rigidity_matrix, self.restrictions_vector, 1)
         
         #Solve for U
-        contour_displacements_vector = solve(contour_global_rigidity_matrix, contour_loads_vector)
+        contour_displacements_vector = np.linalg.solve(contour_global_rigidity_matrix, contour_loads_vector)
 
         #Global U
         displacements_vector = np.zeros((self.nodes_number * 2, 1))
@@ -109,13 +112,13 @@ class Solver:
         target_loads_vector = np.delete(resultant_loads_vector, index, 0)
         
 
-        tensions_vector = np.zeros((len(bars), 1))
-        deformations_vector = np.zeros((len(bars), 1))
-        internal_forces_vector = np.zeros((len(bars), 1))
+        tensions_vector = np.zeros((len(self.bars), 1))
+        deformations_vector = np.zeros((len(self.bars), 1))
+        internal_forces_vector = np.zeros((len(self.bars), 1))
 
         #Tension and Deformation 
-        for i in range(len(bars)):
-            deformation, tension, internal_forces = bars[i].calculate(displacements_vector)
+        for i in range(len(self.bars)):
+            deformation, tension, internal_forces = self.bars[i].calculate(displacements_vector)
             tensions_vector[i] = tension
             deformations_vector[i] = deformation
             internal_forces_vector[i] = internal_forces
@@ -145,28 +148,10 @@ class Solver:
         else:
             print("Solve first")
 
-solver = Solver()
+structure = Structure()
 
-solver.load("entradas/entrada_triangulo")
-solver.plot()
-solver.solve()
-solver.plot()
-solver.write("saidas/saida_triangulo")
-
-solver.load("entradas/entrada_ponte") #http://www.abenge.org.br/cobenge/arquivos/12/artigos/434-Gustavo%20Cunha.pdf
-solver.plot()
-solver.solve()
-solver.plot()
-solver.write("saidas/saida_ponte")
-
-solver.load("entradas/entrada_teste")
-solver.plot()
-solver.solve()
-solver.plot()
-solver.write("saidas/saida_teste")
-
-solver.load("entradas/entrada_teste_extra")
-solver.plot()
-solver.solve()
-solver.plot()
-solver.write("saidas/saida_teste_extra")
+structure.load("entrada_ponte") 
+structure.plot()
+structure.solve()
+structure.plot()
+structure.write("entrada_ponte")
